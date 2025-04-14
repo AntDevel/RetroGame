f4bb26f8446cecd565165da15ae4377207254a33
Gaige Grossman <gaigegrossman@outlook.com>
2025-04-14 09:31:37 -0400

    School

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,97 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.TitleScreen = New System.Windows.Forms.Panel()
+        Me.Map1 = New System.Windows.Forms.Panel()
+        Me.Map1Player = New System.Windows.Forms.PictureBox()
+        Me.PressToPlay = New System.Windows.Forms.Label()
+        Me.Settings = New System.Windows.Forms.Label()
+        Me.Quit = New System.Windows.Forms.Label()
+        Me.TitleScreen.SuspendLayout()
+        Me.Map1.SuspendLayout()
+        CType(Me.Map1Player, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TitleScreen
+        '
+        Me.TitleScreen.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.TitleScreen.Controls.Add(Me.Quit)
+        Me.TitleScreen.Controls.Add(Me.Settings)
+        Me.TitleScreen.Controls.Add(Me.PressToPlay)
+        Me.TitleScreen.Location = New System.Drawing.Point(86, 52)
+        Me.TitleScreen.Name = "TitleScreen"
+        Me.TitleScreen.Size = New System.Drawing.Size(306, 224)
+        Me.TitleScreen.TabIndex = 0
+        '
+        'Map1
+        '
+        Me.Map1.Controls.Add(Me.Map1Player)
+        Me.Map1.Enabled = False
+        Me.Map1.Location = New System.Drawing.Point(493, 197)
+        Me.Map1.Name = "Map1"
+        Me.Map1.Size = New System.Drawing.Size(200, 100)
+        Me.Map1.TabIndex = 1
+        Me.Map1.Visible = False
+        '
+        'Map1Player
+        '
+        Me.Map1Player.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Map1Player.Location = New System.Drawing.Point(62, 18)
+        Me.Map1Player.Name = "Map1Player"
+        Me.Map1Player.Size = New System.Drawing.Size(100, 50)
+        Me.Map1Player.TabIndex = 0
+        Me.Map1Player.TabStop = False
+        '
+        'PressToPlay
+        '
+        Me.PressToPlay.AutoSize = True
+        Me.PressToPlay.Location = New System.Drawing.Point(121, 62)
+        Me.PressToPlay.Name = "PressToPlay"
+        Me.PressToPlay.Size = New System.Drawing.Size(39, 13)
+        Me.PressToPlay.TabIndex = 0
+        Me.PressToPlay.Text = "Label1"
+        '
+        'Settings
+        '
+        Me.Settings.AutoSize = True
+        Me.Settings.Location = New System.Drawing.Point(121, 92)
+        Me.Settings.Name = "Settings"
+        Me.Settings.Size = New System.Drawing.Size(39, 13)
+        Me.Settings.TabIndex = 0
+        Me.Settings.Text = "Label1"
+        '
+        'Quit
+        '
+        Me.Quit.AutoSize = True
+        Me.Quit.Location = New System.Drawing.Point(121, 145)
+        Me.Quit.Name = "Quit"
+        Me.Quit.Size = New System.Drawing.Size(39, 13)
+        Me.Quit.TabIndex = 0
+        Me.Quit.Text = "Label1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Map1)
+        Me.Controls.Add(Me.TitleScreen)
         Me.DoubleBuffered = True
         Me.KeyPreview = True
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.TitleScreen.ResumeLayout(False)
+        Me.TitleScreen.PerformLayout()
+        Me.Map1.ResumeLayout(False)
+        CType(Me.Map1Player, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents TitleScreen As Panel
+    Friend WithEvents Map1 As Panel
+    Friend WithEvents Map1Player As PictureBox
+    Friend WithEvents Quit As Label
+    Friend WithEvents Settings As Label
+    Friend WithEvents PressToPlay As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,121 @@
 ﻿Public Class Form1
     'choosing map properties
     Dim Gamestate As Integer = 0
+    Dim Choices As Integer = 0
+    'KeyDown
+    Dim w As Boolean = False
+    Dim a As Boolean = False
+    Dim s As Boolean = False
+    Dim d As Boolean = False
+    Dim grounded As Boolean = False
+    Dim touching As Boolean
+    Dim isJumping As Boolean
+
     'Load Form
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TitleScreen.Dock = DockStyle.Fill
+        Map1.Dock = DockStyle.Fill
+        PressToPlay.ForeColor = Color.White
+        Dim TopBorder As New PictureBox With {
+            .BackColor = Color.Blue,
+            .Size = Map1Player.Size,
+            .Left = Map1Player.Left,
+            .Top = Map1Player.Top - Map1Player.Height
+            }
+        Dim BottomBorder As New PictureBox With {
+            .BackColor = Color.Blue,
+            .Size = Map1Player.Size,
+            .Left = Map1Player.Left,
+            .Top = Map1Player.Top + Map1Player.Height
+            }
+        Dim LeftBorder As New PictureBox With {
+            .BackColor = Color.Blue,
+            .Size = Map1Player.Size,
+            .Left = Map1Player.Left - Map1Player.Width,
+            .Top = Map1Player.Top
+            }
+        Dim RightBorder As New PictureBox With {
+            .BackColor = Color.Blue,
+            .Size = Map1Player.Size,
+            .Left = Map1Player.Left + Map1Player.Width,
+            .Top = Map1Player.Top
+            }
+        Map1.Controls.Add(RightBorder)
+        Map1.Controls.Add(TopBorder)
+        Map1.Controls.Add(LeftBorder)
+        Map1.Controls.Add(BottomBorder)
 
     End Sub
     'Key Down For Movements
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         Select Case Gamestate
+            Case 0
+
+                For Each LabelControl As Control In TitleScreen.Controls
+
+                    If e.KeyCode = Keys.S Then
+                        If TypeOf LabelControl Is Label Then
+                            LabelControl.ForeColor = Color.Black
+                            If Choices < 7 Then
+                                Choices += 1
+                            Else
+                                Choices = -1
+                            End If
+
+                        End If
+
+
+                    End If
+                    If e.KeyCode = Keys.W Then
+                        If TypeOf LabelControl Is Label Then
+                            LabelControl.ForeColor = Color.Black
+                            If Choices > -1 Then
+                                Choices -= 1
+                            Else
+                                Choices = 7
+                            End If
+                        End If
+
+
+                    End If
+                    'Loading the first Map
+                    Select Case Choices
+                        Case 0
+                            PressToPlay.ForeColor = Color.White
+                        Case 3
+                            Settings.ForeColor = Color.White
+                        Case 6
+                            Quit.ForeColor = Color.White
+                    End Select
+                    PressToPlay.Text = Choices
+                    If e.KeyCode = Keys.Enter Then
+                        Select Case Choices
+                            Case 0
+                                TitleScreen.Enabled = False
+                                TitleScreen.Visible = False
+                                Map1.Visible = True
+                                Map1.Enabled = True
+                            Case 3
+
+                            Case 6
+                                Me.Close()
+                        End Select
+
+
+                    End If
+                Next
             Case 1
                 'GravityMove
+                If e.KeyCode = Keys.W Then
+                    w = True
+                End If
+                If e.KeyCode = Keys.A Then
+                    a = True
+
+                End If
+                If e.KeyCode = Keys.D Then
+                    d = True
+                End If
             Case 2
                 'pacmanMove
             Case 3
@@ -21,6 +127,15 @@
         Select Case Gamestate
             Case 1
                 'GravityMove
+
+
+                If e.KeyCode = Keys.A Then
+                    a = False
+
+                End If
+                If e.KeyCode = Keys.D Then
+                    d = False
+                End If
             Case 3
                 'flyMove
         End Select
@@ -50,7 +165,23 @@
 
     End Sub
 
-    Private Sub MovingPlatforms()
+    Private Sub MovingPlatforms(ByRef Player As PictureBox, ByRef Map As Panel)
+        'Variables
+        Dim PlayerX As Integer = Player.Location.X
+        Dim PlayerY As Integer = Player.Location.Y
+        Dim Velocity As Double = 0
+        Dim MapX As Integer = Map.Location.X
+        Dim MapY As Integer = Map.Location.Y
+        Dim MapMove As Boolean
+
+        'code
+
+        If a Then
+            PlayerX -= 5
+        End If
+        If d Then
+            PlayerX += 5
+        End If
 
     End Sub
     'Pacman
@@ -76,4 +207,9 @@
     Private Sub CloneMoving()
 
     End Sub
+
+    Private Sub PressToPlay_Click(sender As Object, e As EventArgs) Handles PressToPlay.Click
+
+    End Sub
 End Class
+
